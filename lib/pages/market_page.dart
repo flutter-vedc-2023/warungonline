4304b38d29dbb5ef2b86ad5fdb48c5bcc7d7727b
Andik Fajaryanto <145002252+AndikFaj031@users.noreply.github.com>
2023-09-22 14:26:25 +0700

    update page

--- lib/pages/market_page.dart
+++ lib/pages/market_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gojek/components/gopay.dart';
+import 'package:gojek/components/menus.dart';
+import 'package:gojek/components/news.dart';
+import 'package:gojek/theme.dart';
 
 class MarketPage extends StatefulWidget {
   const MarketPage({super.key, required this.warung});
@@ -12,9 +16,19 @@ class _MarketPageState extends State<MarketPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.warung),
+        automaticallyImplyLeading: false,
+        backgroundColor: green2,
+        elevation: 0,
+        toolbarHeight: 71,
       ),
+      body: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [Gopay(), Menus(), News()],
+      )),
     );
   }
 }
